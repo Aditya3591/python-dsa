a1989d1d0cbf2af189c5d8bc9a7c37fb9fb04c5c
Aditya3591 <adityasinghi12345@gmail.com>
2024-02-09 15:47:35 +0530

    [aditya] added:return indices of the two numbers such that they add up to target

---
+++ sum.py
@@ -0,0 +1,21 @@
+def two_sum(nums, target):
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return None
+
+# Test cases
+nums1 = [2, 7, 11, 15]
+target1 = 9
+print(two_sum(nums1, target1))  
+
+nums2 = [3, 2, 4]
+target2 = 6
+print(two_sum(nums2, target2))  
+
+nums3 = [3, 3]
+target3 = 6
+print(two_sum(nums3, target3))  
